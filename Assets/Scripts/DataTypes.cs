3a01caf4b1f57c8c326882919f7caa1a8f8c037c
Daniel Blackford <dblackford0825@sdsu.edu>
2021-11-02 19:08:55 -0700

    Test of Jetbrains Rider

--- Assets/Scripts/DataTypes.cs
+++ Assets/Scripts/DataTypes.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
 public class MazeLink
 {
@@ -51,4 +49,3 @@ public struct GridFocus
         }
     }
 }
-
